8a5c1ec34a6224e5f32714a0363a5ca098b9f5fe
Kai Hayashi <kai.hayashi@twitch.tv>
2014-12-08 17:03:40 -0800

    remove url devoding from the processor to reduce api area

--- parser/nginxParser.go
+++ parser/nginxParser.go
@@ -4,9 +4,7 @@ import (
 	"bytes"
 	"encoding/base64"
 	"encoding/json"
-	"errors"
 	"fmt"
-	"net/url"
 	"strconv"
 	"time"
 
@@ -17,7 +15,6 @@ var MultiEventEscape = []byte{'[', '{'}
 
 type NginxLogParser struct {
 	Reporter reporter.Reporter
-	Escaper  URLEscaper
 }
 
 type parseResult struct {
@@ -27,14 +24,9 @@ type parseResult struct {
 	UUID string
 }
 
-type URLEscaper interface {
-	QueryUnescape([]byte) ([]byte, error)
-}
-
 func BuildSpadeParser(r reporter.Reporter) Parser {
 	return &NginxLogParser{
 		Reporter: r,
-		Escaper:  &ByteQueryUnescaper{},
 	}
 }
 
@@ -92,11 +84,9 @@ func MakeErrorEvent(line *ParseRequest, matches *parseResult) *MixpanelEvent {
 }
 
 func (worker *NginxLogParser) decodeData(matches *parseResult) ([]MixpanelEvent, error) {
+	var err error
 	data := matches.Data
-	data, err := worker.Escaper.QueryUnescape(data)
-	if err != nil {
-		return nil, err
-	}
+
 	// We dont have to allocate a new byte array here because the len(dst) < len(src)
 	n, b64Err := base64.StdEncoding.Decode(data, data)
 	if b64Err != nil {
@@ -146,93 +136,4 @@ func (worker *NginxLogParser) Parse(line *ParseRequest) ([]MixpanelEvent, error)
 		worker.Reporter.IncrementExpected(len(m) - 1)
 	}
 	return m, nil
-}
-
-type StringQueryUnescaper struct{}
-
-func (s *StringQueryUnescaper) QueryUnescape(q []byte) ([]byte, error) {
-	out, err := url.QueryUnescape(string(q))
-	if err != nil {
-		return nil, err
-	}
-	return []byte(out), nil
-}
-
-type ByteQueryUnescaper struct{}
-
-func ishex(c byte) bool {
-	switch {
-	case '0' <= c && c <= '9':
-		return true
-	case 'a' <= c && c <= 'f':
-		return true
-	case 'A' <= c && c <= 'F':
-		return true
-	}
-	return false
-}
-
-func unhex(c byte) byte {
-	switch {
-	case '0' <= c && c <= '9':
-		return c - '0'
-	case 'a' <= c && c <= 'f':
-		return c - 'a' + 10
-	case 'A' <= c && c <= 'F':
-		return c - 'A' + 10
-	}
-	return 0
-}
-
-func (s *ByteQueryUnescaper) QueryUnescape(q []byte) ([]byte, error) {
-	return unescape(q)
-}
-
-func unescape(s []byte) ([]byte, error) {
-	// Count %, check that they're well-formed.
-	n := 0
-	hasPlus := false
-	for i := 0; i < len(s); {
-		switch s[i] {
-		case '%':
-			n++
-			if i+2 >= len(s) || !ishex(s[i+1]) || !ishex(s[i+2]) {
-				s = s[i:]
-				if len(s) > 3 {
-					s = s[0:3]
-				}
-				return nil, errors.New("invalid URL escape")
-			}
-			i += 3
-		case '+':
-			hasPlus = true
-			i++
-		default:
-			i++
-		}
-	}
-
-	if n == 0 && !hasPlus {
-		return s, nil
-	}
-
-	t := make([]byte, len(s)-2*n)
-	j := 0
-	for i := 0; i < len(s); {
-		switch s[i] {
-		case '%':
-			t[j] = unhex(s[i+1])<<4 | unhex(s[i+2])
-			j++
-			i += 3
-		case '+':
-			t[j] = ' '
-			j++
-			i++
-		default:
-			t[j] = s[i]
-			j++
-			i++
-		}
-	}
-	return t, nil
 }
