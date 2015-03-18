ad081a02586d7ab4b646ca5a299189396571162e
Matt Aasted <aasted@twitch.tv>
2015-03-18 14:15:07 -0700

    Use an absolute path for the s3 conf

--- build/scripts/run_spade.sh
+++ build/scripts/run_spade.sh
@@ -19,8 +19,8 @@ STATSD_PREFIX="${CLOUD_APP}.${CLOUD_DEV_PHASE:-${CLOUD_ENVIRONMENT}}.${EC2_REGIO
 mkdir -p ${SPADE_DATA_DIR}/spade_logging ${SPADE_DATA_DIR}/events ${SPADE_DATA_DIR}/upload
 
 export CONFIG_PREFIX="s3://$S3_CONFIG_BUCKET/$VPC_SUBNET_TAG/$CLOUD_APP/$CLOUD_ENVIRONMENT"
-aws s3 cp --region us-west-2 "$CONFIG_PREFIX/conf.sh" conf.sh
-source conf.sh
+aws s3 cp --region us-west-2 "$CONFIG_PREFIX/conf.sh" "$SPADE_DIR/config/conf.sh"
+source "$SPADE_DIR/config/conf.sh"
 
 exec ${SPADE_DIR}/bin/spade -gzipped -spade_dir ${SPADE_DATA_DIR} \
   -config_url ${BLUEPRINT_URL} \
