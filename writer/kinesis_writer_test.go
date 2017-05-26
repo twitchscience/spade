d35c58a5651516df21394f275f736d1fbfe73c28
Doug Ellwanger <dwe@twitch.tv>
2017-05-25 18:24:33 -0700

    Add Firehose throttling code and improve tests
    Test Plan: Ran test.
    
    Reviewers: lydon
    
    Reviewed By: lydon
    
    Subscribers: #scieng
    
    Differential Revision: https://phabricator.internal.justin.tv/D5326

--- writer/kinesis_writer_test.go
+++ writer/kinesis_writer_test.go
@@ -3,12 +3,14 @@ package writer
 import (
 	"encoding/json"
 	"testing"
-	"time"
 
-	"github.com/aws/aws-sdk-go/aws/request"
+	"github.com/aws/aws-sdk-go/aws"
 	"github.com/aws/aws-sdk-go/service/firehose"
+	"github.com/aws/aws-sdk-go/service/firehose/firehoseiface"
 	"github.com/cactus/go-statsd-client/statsd"
+	"github.com/cactus/go-statsd-client/statsd/statsdtest"
 	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
 	"github.com/twitchscience/scoop_protocol/scoop_protocol"
 )
 
@@ -27,7 +29,7 @@ var FirehoseRedshiftStreamTestConfig = []byte(`
             }
         },
         "BufferSize": 1024,
-        "MaxAttemptsPerRecord": 10,
+        "MaxAttemptsPerRecord": 1,
         "RetryDelay": "1s",
         "Globber": {
             "MaxSize": 990000,
@@ -43,73 +45,20 @@ var FirehoseRedshiftStreamTestConfig = []byte(`
     }
   `)
 
-// mocking statsd's Statter
-type statterMock struct{}
-
-func (s statterMock) Inc(string, int64, float32) error                    { return nil }
-func (s statterMock) Dec(string, int64, float32) error                    { return nil }
-func (s statterMock) Gauge(string, int64, float32) error                  { return nil }
-func (s statterMock) GaugeDelta(string, int64, float32) error             { return nil }
-func (s statterMock) Timing(string, int64, float32) error                 { return nil }
-func (s statterMock) TimingDuration(string, time.Duration, float32) error { return nil }
-func (s statterMock) Set(string, string, float32) error                   { return nil }
-func (s statterMock) SetInt(string, int64, float32) error                 { return nil }
-func (s statterMock) Raw(string, string, float32) error                   { return nil }
-func (s statterMock) NewSubStatter(string) statsd.SubStatter              { return nil }
-func (s statterMock) SetPrefix(string)                                    {}
-func (s statterMock) Close() error                                        { return nil }
-
 // mocking firehoseAPI
 type firehoseMock struct {
 	received []map[string]string
+	response *firehose.PutRecordBatchOutput
+	firehoseiface.FirehoseAPI
 }
 
-func (f *firehoseMock) CreateDeliveryStreamRequest(*firehose.CreateDeliveryStreamInput) (*request.Request, *firehose.CreateDeliveryStreamOutput) {
-	return nil, nil
-}
-func (f *firehoseMock) CreateDeliveryStream(*firehose.CreateDeliveryStreamInput) (*firehose.CreateDeliveryStreamOutput, error) {
-	return nil, nil
-}
-func (f *firehoseMock) DeleteDeliveryStreamRequest(*firehose.DeleteDeliveryStreamInput) (*request.Request, *firehose.DeleteDeliveryStreamOutput) {
-	return nil, nil
-}
-func (f *firehoseMock) DeleteDeliveryStream(*firehose.DeleteDeliveryStreamInput) (*firehose.DeleteDeliveryStreamOutput, error) {
-	return nil, nil
-}
-func (f *firehoseMock) DescribeDeliveryStreamRequest(*firehose.DescribeDeliveryStreamInput) (*request.Request, *firehose.DescribeDeliveryStreamOutput) {
-	return nil, nil
-}
-func (f *firehoseMock) DescribeDeliveryStream(*firehose.DescribeDeliveryStreamInput) (*firehose.DescribeDeliveryStreamOutput, error) {
-	return nil, nil
-}
-func (f *firehoseMock) ListDeliveryStreamsRequest(*firehose.ListDeliveryStreamsInput) (*request.Request, *firehose.ListDeliveryStreamsOutput) {
-	return nil, nil
-}
-func (f *firehoseMock) ListDeliveryStreams(*firehose.ListDeliveryStreamsInput) (*firehose.ListDeliveryStreamsOutput, error) {
-	return nil, nil
-}
-func (f *firehoseMock) PutRecordRequest(*firehose.PutRecordInput) (*request.Request, *firehose.PutRecordOutput) {
-	return nil, nil
-}
-func (f *firehoseMock) PutRecord(*firehose.PutRecordInput) (*firehose.PutRecordOutput, error) {
-	return nil, nil
-}
-func (f *firehoseMock) PutRecordBatchRequest(*firehose.PutRecordBatchInput) (*request.Request, *firehose.PutRecordBatchOutput) {
-	return nil, nil
-}
 func (f *firehoseMock) PutRecordBatch(i *firehose.PutRecordBatchInput) (*firehose.PutRecordBatchOutput, error) {
 	for _, v := range i.Records {
 		var unpacked map[string]string
 		_ = json.Unmarshal(v.Data, &unpacked)
 		f.received = append(f.received, unpacked)
 	}
-	return &firehose.PutRecordBatchOutput{}, nil
-}
-func (f *firehoseMock) UpdateDestinationRequest(*firehose.UpdateDestinationInput) (*request.Request, *firehose.UpdateDestinationOutput) {
-	return nil, nil
-}
-func (f *firehoseMock) UpdateDestination(*firehose.UpdateDestinationInput) (*firehose.UpdateDestinationOutput, error) {
-	return nil, nil
+	return f.response, nil
 }
 
 func TestConfigValidation(t *testing.T) {
@@ -138,61 +87,30 @@ func TestRedshiftStreamAndStreamValidation(t *testing.T) {
 
 func TestRedshiftStreamMode(t *testing.T) {
 	inputMaps := []map[string]string{
-		map[string]string{
-			"country":   "US",
-			"device_id": "xyz123",
-		},
-		map[string]string{
-			"country":   "",
-			"device_id": "",
-		},
-		map[string]string{
-			"country":   "CA",
-			"device_id": "\x00",
-		},
-		map[string]string{
-			"country":   "CA",
-			"device_id": "xyz\x00123",
-		},
-		map[string]string{
-			"country":   "CA",
-			"device_id": "\x00\x00\x00\x00\x00",
-		},
+		{"country": "US", "device_id": "xyz123"},
+		{"country": "", "device_id": ""},
+		{"country": "CA", "device_id": "\x00"},
+		{"country": "CA", "device_id": "xyz\x00123"},
+		{"country": "CA", "device_id": "\x00\x00\x00\x00\x00"},
 	}
 
 	expectedMaps := []map[string]string{
-		map[string]string{
-			"country":   "US",
-			"device_id": "xyz123",
-		},
-		map[string]string{
-			"country":   "",
-			"device_id": "",
-		},
-		map[string]string{
-			"country":   "CA",
-			"device_id": "",
-		},
-		map[string]string{
-			"country":   "CA",
-			"device_id": "xyz123",
-		},
-		map[string]string{
-			"country":   "CA",
-			"device_id": "",
-		},
+		{"country": "US", "device_id": "xyz123"},
+		{"country": "", "device_id": ""},
+		{"country": "CA", "device_id": ""},
+		{"country": "CA", "device_id": "xyz123"},
+		{"country": "CA", "device_id": ""},
 	}
 
 	config := scoop_protocol.KinesisWriterConfig{}
 	_ = json.Unmarshal(FirehoseRedshiftStreamTestConfig, &config)
 
 	// create mock objects
-	mockInnerStatter := statterMock{}
 	mockStatter := &Statter{
-		statter:   mockInnerStatter,
+		statter:   &statsd.NoopClient{},
 		statNames: map[int]string{},
 	}
-	mockFirehose := firehoseMock{}
+	mockFirehose := firehoseMock{response: &firehose.PutRecordBatchOutput{}}
 	batchWriter := &FirehoseBatchWriter{&mockFirehose, &config, mockStatter}
 
 	// matching input format
@@ -208,8 +126,51 @@ func TestRedshiftStreamMode(t *testing.T) {
 	// check for expected values
 	for i := range mockFirehose.received {
 		for k := range mockFirehose.received[i] {
-			assert.Equal(t, mockFirehose.received[i][k], expectedMaps[i][k],
+			assert.Equal(t, expectedMaps[i][k], mockFirehose.received[i][k],
 				"output was not equal for input %v key %s", i, k)
 		}
 	}
+}
+
+func TestRedshiftStreamStatting(t *testing.T) {
+	inputMaps := []map[string]string{
+		{"country": "US", "device_id": "xyz123"},
+		{"country": "", "device_id": ""},
+	}
+
+	config := scoop_protocol.KinesisWriterConfig{}
+	_ = json.Unmarshal(FirehoseRedshiftStreamTestConfig, &config)
+
+	// create mock objects
+	statRecorder := statsdtest.NewRecordingSender()
+	statsdClient, _ := statsd.NewClientWithSender(statRecorder, "")
+	mockStatter := &Statter{
+		statter:   statsdClient,
+		statNames: generateStatNames("stream"),
+	}
+	mockFirehose := firehoseMock{response: &firehose.PutRecordBatchOutput{
+		RequestResponses: []*firehose.PutRecordBatchResponseEntry{
+			{ErrorCode: aws.String("ServiceUnavailableException")},
+			{ErrorCode: aws.String("InternalFailure")},
+		},
+	}}
+	batchWriter := &FirehoseBatchWriter{&mockFirehose, &config, mockStatter}
+
+	// matching input format
+	inputBatch := [][]byte{}
+	for _, m := range inputMaps {
+		b, _ := json.Marshal(m)
+		inputBatch = append(inputBatch, b)
+	}
+
+	// send it
+	batchWriter.SendBatch(inputBatch)
+
+	stats := statRecorder.GetSent()
+	require.Equal(t, 5, len(stats))
+	assert.Equal(t, "kinesiswriter.stream.putrecords.attempted 1 ", stats[0].String())
+	assert.Equal(t, "kinesiswriter.stream.putrecords.length 2 ", stats[1].String())
+	assert.Equal(t, "kinesiswriter.stream.records_failed.internal_error 1 ", stats[2].String())
+	assert.Equal(t, "kinesiswriter.stream.records_failed.unknown_reason 1 ", stats[3].String())
+	assert.Equal(t, "kinesiswriter.stream.records_dropped 2 ", stats[4].String())
 }
