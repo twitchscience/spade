0259ec24c4c008f742e5d4bda08362b9cd04a5c2
Michael Perrone <mperrone@twitch.tv>
2017-06-05 13:23:38 -0700

    add code coverage report generation

--- build.sh
+++ build.sh
@@ -1,8 +1,6 @@
 #!/bin/bash --
 set -euo pipefail
 
-# ossareh(20150109): Perhaps use something like:
-# http://stackoverflow.com/questions/192249/how-do-i-parse-command-line-arguments-in-bash
 PROJECT=$1
 BRANCH=$2
 SOURCE_AMI=$3
@@ -10,7 +8,6 @@ VPC=$4
 SUBNET=$5
 SECURITY_GROUP=$6
 
-# I hate boolean args, but I'm not sure how to handle this.
 USE_PRIVATE_IP=${7:-"false"}
 
 export GOARCH=amd64
@@ -18,7 +15,7 @@ export GOOS=linux
 
 # godep restore
 go vet ./...
-go test -v ./...
+bash run_tests.sh
 go install -v ./...
 gometalinter --disable gas --disable gocyclo --disable dupl --deadline=90s --enable unused
 
